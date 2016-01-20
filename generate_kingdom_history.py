e389be2b4affabf20012a7568243780dde1d99f4
Justin McGuire <jm@landedstar.com>
2016-01-19 20:53:28 -0500

    add tag info to config. finished generate script.

--- generate_kingdom_history.py
+++ generate_kingdom_history.py
@@ -2,7 +2,9 @@
 """
 generate_kingdom_history.py
 
-Generate the history of a kingdom, using Johnn Four's playing card method, from "RPT#681: HOW TO CREATE YOUR KINGDOM’S HISTORY" (https://roleplayingtips.com/rptn/rpt681-create-kingdoms-history/).
+Generate the history of a kingdom, using Johnn Four's playing card method, from
+"RPT#681: HOW TO CREATE YOUR KINGDOM’S HISTORY"
+(https://roleplayingtips.com/rptn/rpt681-create-kingdoms-history/).
 
 Created by Justin McGuire <jm@landedstar.com>
 Content copyright Johnn Four <roleplayingtips.com>
@@ -17,14 +19,14 @@ class KingdomEvent:
   config
 
   def __init__(self):
-    self.description = ''
-    self.tags        = ''
-    self.tag_info    = ''
+    self.desc        = ''
+    self.tag         = ''
+    self.tag_desc    = ''
     self.growing     = False
 
     self.load_config
 
-    self.load_random_event
+    self.get_random_event
 
   def load_config:
     """load the config file into the static config variable, but only once"""
@@ -32,7 +34,7 @@ class KingdomEvent:
       with open("history.yaml") as file:
         self.config = yaml.load(file)
 
-  def load_random_event:
+  def get_random_event:
     """get a random event, and load it into our object"""
     rank = random_to(13)
     suit = random_to(4)
@@ -44,12 +46,33 @@ class KingdomEvent:
 
     event = config['cards'][rank][suit]
 
-    self.get_tags
+    self.process_tags
 
-  def get_tags
-    """look for and remove tags in the description"""
-    pass
-  
+  def process_tags
+    """look for tags, remove them from the description, and manage them"""
+    #look for a " +TAG_THING " in description
+    m = re.search(r'\s*(\+\w+)', self.desc)
+    tag = m.group(0):
+
+    if not tag:
+      return
+
+    #remove the tag from the description
+    self.desc.replace(tag, '')
+
+    #get a print-friendly tag name
+    tag = re.sub(r'\s*\+(\w+)\s*', r'\1', tag)
+    self.tag = tag
+    self.tag.replace('_', ' ').title
+
+    #get our tag description
+    self.tag_desc = self.config['tags'][tag]['description']
+    option = random.choice(self.config['tags'][tag]['options'])
+    option2 = random.choice(self.config['tags'][tag]['options2'])
+    if option:
+      self.tag_desc += " " + option
+    if option2:
+      self.tag_desc += " " + option2
 
 
 class Kingdom:
@@ -67,11 +90,17 @@ class Kingdom:
       else
         self.growth--
 
+    for event in events:
+      print "%s" % event.desc
+      if event.tag:
+        print " %s (%s)" % (event.tag, event.tag_desc)
+
+
 def main(num_events=5):
   kingdom = Kingdom(num_events)
 
 def random_to(max):
-  """chose a number form 1 to max"""
+  """chose a number from 1 to max"""
   return random.randint(1, picklist)
 
 if __name__ == '__main__':
